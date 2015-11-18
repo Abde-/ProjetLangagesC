22869659a4a8f45047ec2692afd2fccd01473790
Abdeselam El-Haman <aelhaman@gmail.com>
2015-11-19 00:05:41 +0100

    [MàJ] Polynome, methode degree

--- polynome.hpp
+++ polynome.hpp
@@ -2,12 +2,23 @@
 #define _POLYNOME_H_
 
 template <typename Elem>
-class Polynome{
-	int _degre;
+class Polynome: Vector<Elem>{
+	int _degree;
 public:
-	virtual int getDegree() = 0;
+	virtual int getDegree();
 
-	 
+	virtual void print(ostream&) const override;
+	virtual void input(istream&) const override;
 };
+
+template <typename Elem>
+int Polynome<Elem>::getDegree(){
+	// get degree for positive (how to do negative?)
+	int degree = 0;
+	for(size_t i = 0; i < this->getSize(); ++i)
+		if (this->getVal()[i]) degree = i;
+
+	return degree;
+}
 
 #endif	/* _POLYNOME_H_ */
